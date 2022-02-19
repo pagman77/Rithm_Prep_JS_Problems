2bcc545f30da445e2bfcfcb9ac4cc3f71b9a47ab
Michael Paglione <pagman777@gmail.com>
2022-02-18 21:07:34 -0500

    solve two more problems

--- padEnd.js
+++ padEnd.js
@@ -1,19 +1,26 @@
 function padEnd(str, num, pad){
-  if (num < str.length){
-    return str
-  }
 
-  if (pad === undefined) {
-    while (str.length < num) {
-      str += " ";
+if (num < str.length){
+  return str
+}
+let padAmount = num - str.length
+
+if (pad !== undefined){
+  while(padAmount > 0)
+    for (let char of pad){
+      if(padAmount === 0){
+        break
+      }
+      str += char
+      padAmount--
     }
-  } else {
 
 
+}else{
+    while (padAmount > 0){
+    str += " ";
+    padAmount--
+    }
   }
-return str
-};
-
-function repeat(){
-
+    return str
 }
--- validAnagram.js
+++ validAnagram.js
@@ -1,3 +1,10 @@
-function validAnagram(){
-  // add whatever parameters you deem necessary - good luck!
+function validAnagram(str1, str2){
+
+ if (str1.length !== str2.length){
+   return false
+ }
+let sort1 = str1.split("").sort();
+let sort2 = str2.split('').sort();
+
+return sort1.every((x,i) => x === sort2[i])
 }
